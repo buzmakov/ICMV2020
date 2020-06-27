69103e9fdbe3d23ff628169e1d509319b1d5e23e
Alexey Buzmakov <buzmakov@gmail.com>
2020-06-27 17:12:20 +0300

    working with SART. Fails with FBP due noralization.

--- roi-tomo.py
+++ roi-tomo.py
@@ -1,26 +1,26 @@
 # %%*- coding: utf-8 -*-
 # % matplotlib inline
 
-# %%
-from tomopy.misc.phantom import shepp2d, shepp3d
 import matplotlib
 import numpy as np
-import h5py
 import pylab as plt
-from tqdm import tqdm
-from tomo.recon.astra_utils import astra_recon_2d_parallel, astra_fp_2d_parallel
-from cv2 import medianBlur
 from scipy import interpolate as interp
+from tomopy.misc.phantom import shepp2d
+from tqdm import tqdm
+from numba import njit, jit
+from tomo.recon.astra_utils import astra_recon_2d_parallel, astra_fp_2d_parallel
+import tomopy
 
 matplotlib.rcParams.update({'font.size': 16})
 # %%
-data = np.squeeze(shepp2d(256)).astype('float32')
+data = np.squeeze(shepp2d(128)).astype('float32')
 data /= data.max()
 data = np.pad(data, data.shape[0] // 4, mode='constant')
-angles = np.arange(0, 180, 1)
+angles = np.arange(0, 180, 0.1)
 
 origin_sinogram = astra_fp_2d_parallel(data, angles)
 rec = astra_recon_2d_parallel(origin_sinogram, angles)
+
 
 # %%
 plt.figure(figsize=(12, 12))
@@ -41,7 +41,7 @@ def create_circle_mask(size):
     X, Y = np.meshgrid(np.arange(size), np.arange(size))
     X -= size // 2
     Y -= size // 2
-    mask = (X ** 2 + Y ** 2) < (size // 2) ** 2 - 10
+    mask = (X ** 2 + Y ** 2) < (size // 2 - 5) ** 2
     return mask
 
 
@@ -69,26 +69,33 @@ def interpolate_sino(image, mask):
     return z
 
 
-# plt.figure(figsize=(15, 10))
-# plt.imshow(interpolate_sino(sino, mask))
-# plt.show()
+def show_sino_rec(sino, recon):
+    plt.figure(figsize=(15, 10))
+    plt.subplot(121)
+    plt.imshow(sino, cmap=plt.cm.gray)
+    plt.axis('tight')
+    plt.subplot(122)
+    plt.imshow(recon[50:-50, 50:-50], vmin=0, vmax=1,
+               cmap=plt.cm.gray)
+    plt.show()
 
-def recon_with_mask(sinogram: np.ndarray, angles: np.ndarray, mask: np.ndarray, niters=5, method=[['FBP_CUDA']]):
+def recon_with_mask(sinogram: np.ndarray, angles: np.ndarray, mask: np.ndarray, niters, method):
     assert sinogram.shape == mask.shape
-    circ_mask = create_circle_mask(sinogram.shape[1])
     t_sino = sinogram.copy() * mask
     # t_sino = interpolate_sino(t_sino, mask)
-    rec = np.zeros_like(circ_mask)
+    rec = np.zeros((sinogram.shape[1], sinogram.shape[1]), dtype='float32')
+    # k0 = np.sum(t_sino[mask])
     for i in tqdm(range(niters)):
-        # t_sino = fix_radon(t_sino, mask)
-        for j in range(i+1):
-            rec = astra_recon_2d_parallel(t_sino, angles, method=method, data=None)
-            # rec *= circ_mask
-            # rec[rec < 0] /= 2
-            t_sino = astra_fp_2d_parallel(rec, angles)
-            t_sino[mask] = sinogram[mask]
-            # t_sino[np.invert(mask)] *= np.random.normal(
-            #     1.0, ((niters-i)/niters)*1e-2, np.sum(np.invert(mask)))
+        rec = astra_recon_2d_parallel(t_sino, angles, method=method, data=None)
+        t_sino = astra_fp_2d_parallel(rec, angles)
+        # k1 = np.sum(t_sino)
+        # kk = k1 / k0
+        # k0 = k1
+        # t_sino /= kk
+        t_sino[mask] = sinogram[mask]
+        if i % 10 == 0:
+            show_sino_rec(t_sino, rec)
+            # print(kk)
     return rec, t_sino
 
 
@@ -97,37 +104,19 @@ begin_stripe = mask.shape[1] // 4 + 13
 # mask[:mask.shape[0] // 2, begin_stripe:begin_stripe + 10] = False
 mask[:, begin_stripe:begin_stripe + 10] = False
 sino = create_masked_sinogram(origin_sinogram, mask, 'constant')
+
+
 mask_recon, res_sino = recon_with_mask(sino, angles, mask,
-                                       niters=30,
+                                       niters=1000,
+                                       # method=[['FBP_CUDA', 1]],
                                        method=[['SART_CUDA', 50],
-                                               ['SIRT_CUDA', 100],
-                                               ])
-
-# plt.figure(figsize=(15, 10))
-# plt.subplot(231)
-# plt.imshow(data, cmap=plt.cm.gray)
-# plt.subplot(232)
-# plt.imshow(sino, cmap=plt.cm.gray)
-# plt.colorbar(orientation='horizontal')
-# plt.subplot(233)
-# plt.imshow(res_sino, cmap=plt.cm.gray)
-# plt.colorbar(orientation='horizontal')
-#
-# plt.subplot(234)
-# plt.imshow(mask_recon, cmap=plt.cm.gray)
-# plt.subplot(235)
-# plt.imshow(data-mask_recon, cmap=plt.cm.viridis)
-# plt.subplot(236)
-# plt.imshow(mask, cmap=plt.cm.gray)
-# plt.show()
+                                               ['SIRT_CUDA', 100]]
+                                       )
 
 
-plt.figure(figsize=(15, 10))
+show_sino_rec(res_sino, mask_recon)
 
-plt.subplot(121)
-plt.imshow(res_sino, cmap=plt.cm.gray)
-plt.axis('tight')
-plt.subplot(122)
-plt.imshow(mask_recon[50:-50, 50:-50], vmin=0, vmax=1,
-           cmap=plt.cm.gray)
-plt.show()
+# %%
+# method = [['FBP_CUDA']]
+# final_rec = astra_recon_2d_parallel(res_sino, angles, method=method, data=None)
+# show_sino_rec(res_sino, final_rec)
